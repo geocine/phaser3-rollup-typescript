dfc19405138ca213b7759426613f7c74ea3bef94
Carlos Montiers A <cmontiers@gmail.com>
2020-07-11 22:28:42 -0400

    Replace deprecated dependency rollup-plugin-commonjs by @rollup/plugin-commonjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import commonjs from 'rollup-plugin-commonjs';
+import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import alias from '@rollup/plugin-alias';
